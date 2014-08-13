f19cbdcb25a8d0fa676bf7aecef1ca1781352558
Randy <Randy@ecinternet.com>
2014-08-12 18:42:22 -0700

    Update.

--- header.php
+++ header.php
@@ -4,10 +4,10 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Bootstrap 101 Template</title>
+		<title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
 
 		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -15,5 +15,32 @@
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+		
+		<?php wp_enqueue_script("jquery"); ?>
+		<?php wp_head(); ?>
 	</head>
 	<body>
+		<div class="blog-masthead">
+			<div class="container">
+				<nav class="blog-nav">
+					<?php
+						$pages = get_pages(array( 'exclude' => 95 ));
+						foreach ( $pages as $page )
+						{
+							$text = '<a class="blog-nav-item" href="' . get_page_link( $page->ID ) . '">';
+							$text .= $page->post_title;
+							$text .= '</a>';
+							
+							echo $text;
+						}
+					?>
+				</nav>
+			</div>
+		</div>
+
+		<div class="container">
+
+			<div class="blog-header">
+				<h1 class="blog-title"><a class="brand" href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+				<p class="lead blog-description">I didn't know I couldn't do that.</p>
+			</div>
